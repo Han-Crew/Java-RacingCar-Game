4a9178c79f5c3089bcf99f8d613c79fcac4ae931
woobeen.lim <drogba02@naver.com>
2021-04-23 16:48:05 +0900

    Add - GameStringUtils 추가 (Game 관련 String 메서드)

---
+++ src/main/java/racingcar/view/GameStringUtils.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class GameStringUtils {
+    public static String getWinningCarsToString(List<String> winningCars) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i=0; i< winningCars.size(); i++) {
+            stringBuffer.append(winningCars.get(i));
+            stringBuffer.append(",");
+        }
+
+        String result = stringBuffer.toString();
+        return result.substring(0 , result.length() - 1);
+    }
+}
